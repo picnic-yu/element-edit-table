3fc0c452d529cbe6c91b1b8381f2460879f7f092
陈博宇 <terry.chen@cloudkeeper.cn>
2019-05-28 17:45:22 +0800

    修改v-tale

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,6 @@
 <template>
 <div>
-  <!-- <v-table-extends :data="tableData3" table-ref='multipleTable1' @keywordDown='keywordDown' @handleDeleteRow='handleDeleteRow' @handleEditRow='handleEditRow' @handleCreate='handleCreate'   ref="multipleTable" highlight-current-row>
-		<v-table-column type="selection" width="55" ></v-table-column>
-		<v-table-column type="index" label="序号" width="60"></v-table-column>
-		<v-table-column prop="sex" label="性别" width="100" :formatter="formatSex"></v-table-column>
-		<v-table-column prop="date" label="日期"></v-table-column>
-		<v-table-column prop="name" label="姓名"></v-table-column>
-		<v-table-column prop="IsAudit" :isEdit= 'checkoutStatue'  :formatter="formatterColumn" label="审核状态" ></v-table-column>
-		<v-table-column prop="address" label="地址"></v-table-column>
-	</v-table-extends> -->
-  <!-- <v-table-extends :data="tableData3" table-ref='multipleTable2' ref="multipleTable" highlight-current-row>
-		<v-table-column type="selection" width="55" ></v-table-column>
-		<v-table-column type="index" label="序号" width="60"></v-table-column>
-		<v-table-column prop="sex" label="性别" width="100" :formatter="formatSex"></v-table-column>
-		<v-table-column prop="date" label="日期"></v-table-column>
-		<v-table-column prop="name" label="姓名"></v-table-column>
-		 <v-table-column prop="IsAudit"  :formatter="formatterColumn" label="审核状态" ></v-table-column>
-		<v-table-column prop="address" label="地址"></v-table-column>
-	</v-table-extends> -->
-  <!-- <el-input v-model="input" placeholder="请输入内容"></el-input> -->
-  <v-table :columns='columns' :data='data'></v-table>
+  <v-table :columns='columns' :data='data' @handleDelete='handleDelete'></v-table>
 </div>
 	
 </template>
@@ -31,36 +12,24 @@ export default {
   },  
 	data() {
 		return {
-      checkoutStatue:true,
-      input:'',
-      tableData3: [{
-        id:'1',
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄',
-        IsAudit:0,
-        sex:'1',
-        isEdit:false,
-      }, {
-        id:'2',
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄',
-        IsAudit:1,
-        isEdit:false,
-        sex:'0'
-      }, {
-        id:'3',
-        isEdit:true,
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄',
-        IsAudit:10,
-        sex:'-1'
-      }],
       columns:[
-        { field: "type", title: "远程类型", width: 120 },
-        { field: "addport", title: "连接地址", width: 150 },
+        { 
+          field: "type", 
+          title: "远程类型", 
+          width: 120,
+          type:'select',
+          option:[
+            {
+              code:'1',
+              value:'公司'
+            },
+            {
+              code:'2',
+              value:'住宅'
+            }
+          ] 
+        },
+        { field: "addport", title: "连接地址", width: 150,type:'date' },
         { field: "user", title: "登录用户", width: 120 },
         { field: "pwd", title: "登录密码", width: 220 },
         { field: "info", title: "其他信息" }
@@ -71,42 +40,11 @@ export default {
 	mounted() {
 	},
 	methods: {
-		formatSex: function (row, column, cellValue, index) {
-			return row.sex == 1 ? '男' : row.sex == 0 ? '女' : '未知';
+    handleDelete(row){
+      console.log(row);
+      console.log(this.data)
     },
-
-    keywordDown(index,row){
-      console.log(index,row)
-    },
-    handleCreate(){
-      console.log(22222222)
-    },
-    handleEditRow(row){
-      console.log(row,'row')
-      console.log(this.tableData3)
-    },
-    handleDeleteRow(row){
-      console.log(row)
-    },
-		//状态改成汉字
-    formatterColumn(row, column) {
-      switch(row.IsAudit){
-        case 0:
-          return '未通过'
-          break
-        case 1:
-          return '审核通过'
-          break
-        case 10:
-          return '待审核'
-          break
-        case 9:
-          return '草稿'
-          break
-        default:
-          return '未知'
-      }
-    }
+		
   }
 }
 </script>
--- src/components/v-table.vue
+++ src/components/v-table.vue
@@ -1,8 +1,6 @@
 <template>
     <div>
         <el-form label-position="right" status-icon :rules="rules" :model="editTableOption.sel" ref="ruleForm">
-            
-        
             <v-table-extends  
             :data="editTableOption.data" 
             border style="width: 100%" 
@@ -16,13 +14,30 @@
             @row-click="handleCurrentChange" 
             highlight-current-row>
                 <el-table-column type="index"></el-table-column>
-                <el-table-column v-for="(v,i) in editTableOption.columns" :key='i' :prop="v.field" :label="v.title" :width="v.width">
+                <el-table-column v-for="(v,i) in editTableOption.columns" :key='i' :prop="v.field" :label="v.title" >
                     <template slot-scope="scope">
                         <span v-if="scope.row.isSet">
-                            <el-form-item :prop='v.field' >
+                            <el-form-item :prop='v.field' v-if='v.type == "select"'>
+                                <el-select v-model="editTableOption.sel[v.field]" placeholder="请选择">
+                                    <el-option
+                                    v-for="item in v.option"
+                                    :key="item.value"
+                                    :label="item.value"
+                                    :value="item.code">
+                                    </el-option>
+                                </el-select>
+                            </el-form-item>
+                            <el-form-item :prop='v.field' v-else-if='v.type == "date"'>
+                                <el-date-picker
+                                    v-model="editTableOption.sel[v.field]"
+                                    type="date"
+                                    placeholder="选择日期">
+                                </el-date-picker>
+                            </el-form-item>
+                            
+                            <el-form-item :prop='v.field' v-else>
                                 <el-input  v-model="editTableOption.sel[v.field]" :maxlength=50></el-input>
                             </el-form-item>
-
                             <!-- <el-input  placeholder="请输入内容" v-model="editTableOption.sel[v.field]">
                             </el-input> -->
                         </span>
@@ -38,18 +53,16 @@
 export default {
     props:{
         columns:Array,
-        data:Array
+        data:Array,
+        formRules:Object,
+
     },
     data(){
         return{
             editTableOption: {
                 sel: null,//选中行   
                 columns: [
-                    { field: "type", title: "远程类型", width: 120 },
-                    { field: "addport", title: "连接地址", width: 150 },
-                    { field: "user", title: "登录用户", width: 120 },
-                    { field: "pwd", title: "登录密码", width: 220 },
-                    { field: "info", title: "其他信息" }
+                   
                 ],
                 data: [],
             },
@@ -74,21 +87,13 @@ export default {
         
     },
     methods: {
-        //读取表格数据
-        readMasterUser() {
-            //根据实际情况，自己改下啊 
-            this.editTableOption.data.map(i => {
-                // i.id = generateId.get();//模拟后台插入成功后有了id
-                i.isSet=false;//给后台返回数据添加`isSet`标识
-                return i;
-            });
-        },
+    
         handleCurrentChange(row, event, column){
             console.log(row,event,column)
         },
         keywordDown(index,row){
             if(!row.isSet) return;
-            this.pwdChange(row,index,true)
+            this.pwdChange(row,index)
         },
         handleCreate(){
             this.addMasterUser();
@@ -96,13 +101,14 @@ export default {
         handleEditRow(row){
             
             // console.log(this.tableData3)
-            // this.editTableOption.data[row.index].isSet = true;
+            this.editTableOption.data[row.index].isSet = true;
+            
             console.log(row,'row')
-            this.pwdChange(row,row.index)
-            // this.pwdChange(row,index,true)
+            this.pwdChange(row,row.index,true)
         },
         handleDeleteRow(row){
             console.log(row)
+            this.$emit('handleDelete',row);
         },
         //添加账号
         addMasterUser() {
@@ -115,41 +121,39 @@ export default {
             this.columns.forEach((item) => {
                 j[item.field] = '';
                 j['isSet'] = true;
+                j['index'] = index;
             });
-            // let j = {  "type": "", "addport": "", "user": "", "pwd": "", "info": "", "isSet": true, "_temporary": true,index };
+            
             this.editTableOption.data.push(j);
             
             this.editTableOption.sel = JSON.parse(JSON.stringify(j));
+            console.log(this.editTableOption,'this.editTableOption')
         },
         //修改
         pwdChange(row, index, cg) {
             //点击修改 判断是否已经保存所有操作
             for (let i of this.editTableOption.data) {
                 if (i.isSet && i.id != row.id) {
+                    console.log(2222222222222222)
                     // this.$message.warning("请先保存当前编辑项");
                     return false;
                 }
             }
-            // console.log(row)
-            // //是否是取消操作
-            // if (!cg) {
-            //     if (!this.editTableOption.sel.id) this.editTableOption.data.splice(index, 1);
-            //     return row.isSet = !row.isSet;
-            // }
+            // 编辑
+            if (cg) return row.isSet = true;
             //提交数据
             if (row.isSet) {
                 let self = this;
                 this.$refs['ruleForm'].validate(valid => {
                     if (valid) {
                         //根据状态dialogStatus判断是新增还是更新
-                        let data = JSON.parse(JSON.stringify(self.editTableOption.sel));
+                        let data = JSON.parse(JSON.stringify(this.editTableOption.sel));
                         for (let k in data) row[k] = data[k];
                         // app.$message({
                         //     type: 'success',
                         //     message: "保存成功!"
                         // });
                         //然后这边重新读取表格数据
-                        self.readMasterUser();
                         row.isSet = false;
                     } else {
                             alert(222)
